917a2ba670f3078cf4b5f25d949fa531fd416e6d
Vikas Upadhyay <vikasu899@gmail.com>
2025-03-21 11:26:04 +0530

    Throw,Throws and finally example along with customized exception

--- src/customexception/UserErrorException.java
+++ src/customexception/UserErrorException.java
@@ -3,7 +3,6 @@ package customexception;
 public class UserErrorException extends Exception{
     public UserErrorException() {
     }
-
     public UserErrorException(String message) {
         super(message);
     }
